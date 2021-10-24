09010f6cdf4a4b8dc9a5bdfe074521f79034a288
daminnaa-d <dilaratemirtay@gmail.com>
2021-10-24 23:26:50 +0600

    Adding Lombok and Swagger2

--- src/main/java/com/example/subscription/controller/Ordercontroller.java
+++ src/main/java/com/example/subscription/controller/Ordercontroller.java
@@ -2,29 +2,37 @@ package com.example.subscription.controller;
 
 import com.example.subscription.model.Order;
 import com.example.subscription.service.OrderService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/orders")
+@Api(value = "Order Controller class", description = "This class allows to interact with Order object")
 public class Ordercontroller {
     @Autowired
     private OrderService orderService;
 
     @GetMapping("{id}")
+    @ApiOperation(value = "Method to get order by ID", response = List.class)
     public ResponseEntity<?> getOrderById(@PathVariable int id){
         return ResponseEntity.ok(orderService.getOrderById(id));
     }
 
 
     @PostMapping("")
+    @ApiOperation(value = "Method to create a new order", response = List.class)
     public void createOrder (@RequestBody Order order){
         orderService.createOrder(order);
     }
 
     @PostMapping("/update/{id}")
+    @ApiOperation(value = "Method to update order", response = List.class)
     public void updateOrder(@PathVariable int id, @RequestBody String subId){
         orderService.addSubToOrder(id, Integer.parseInt(subId));
     }
--- src/main/java/com/example/subscription/controller/SubscriptionController.java
+++ src/main/java/com/example/subscription/controller/SubscriptionController.java
@@ -1,19 +1,25 @@
 package com.example.subscription.controller;
 
 import com.example.subscription.service.SubscriptionService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/subs")
+@Api(value = "Subscription Controller class", description = "This class allows to interact with Subscription object")
 public class SubscriptionController {
     @Autowired
     private SubscriptionService subscriptionService;
 
     @GetMapping("")
+    @ApiOperation(value = "Method to get list of subscriptions", response = List.class)
     public ResponseEntity<?> getAllSubs(){
         return ResponseEntity.ok(subscriptionService.getAllSubs());
     }
